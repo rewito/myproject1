557b60068ad8d41c75ee1ff9a1c229610951ac63
John Doe <johndoe@example.com>
2023-01-11 21:16:16 +0300

    initial commit

---
+++ test.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func test(o int) {
+
+	switch o {
+	case 0:
+		fmt.Println("Zero")
+	case 1:
+		fmt.Println("One")
+	case 2:
+		fmt.Println("Two")
+	case 3:
+		fmt.Println("Three")
+	case 4:
+		fmt.Println("Four")
+	case 5:
+		fmt.Println("Five")
+	case 6:
+		fmt.Println("Six")
+	case 7:
+		fmt.Println("Seven")
+	case 8:
+		fmt.Println("Eight")
+	case 9:
+		fmt.Println("Nine")
+	case 10:
+		fmt.Println("Ten")
+	default:
+		fmt.Println("This is not a number or a number between 0 and 10")
+	}
+
+}
+func main() {
+	var i, m, n int
+	fmt.Scan(&i)
+	fmt.Scan(&m)
+	fmt.Scan(&n)
+	test(i)
+	test(m)
+	test(n)
+
+}
